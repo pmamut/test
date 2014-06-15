2debffbe3c53e6b4a2fd104ba8546f8edd47f4d6
Pavel Mamut <pavel@versionone.com>
2014-06-15 01:57:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ simply.fullscreen(true);
 simply.style('mono');
 simply.text({}, true);
 
+var script = 'http://cdn.socket.io/socket.io-1.0.4.js';
+
 simply.on('singleClick', function(e){
 	if (e.button === 'select')
-		ajax('http://cdn.socket.io/socket.io-1.0.4.js', function(data){
-			simply.body(data, true);
-			simply.scrollable(true);
+		simply.body('fetching ' + script)
+		ajax(script, function(data){
+			simply.body('ghot it!');
+			//simply.scrollable(true);
 		}, function(){
 			simply.vibe();
 		});
